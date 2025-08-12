c2a510436e1555e17c474c7e8a350ea05dea6576
petrovica <petrovica@ae.com>
2025-08-12 11:23:10 +0200

    Initial commit - update existing and add new dependencies and create a clean starting point with the project architeture set up

--- twitter-clone/src/index.tsx
+++ twitter-clone/src/index.tsx
@@ -1,17 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import "./index.css";
+import ReactDOM from "react-dom/client"; // Note the /client import
+import App from "./pages/App";
+import { BrowserRouter, Route, Routes } from "react-router";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import AuthLayout from "./layouts/AuthLayout";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
 );
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+        <Route element={<AuthLayout />}>
+          <Route path="login" element={<Login />} />
+          <Route path="signup" element={<Signup />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
---
+++ twitter-clone/src/pages/App.tsx
@@ -0,0 +1,5 @@
+const App: React.FC = () => {
+  return <div>Aka</div>;
+};
+
+export default App;
